91605b6a45929abfacdc815a18b58d6a79191255
Twist-eight <yuukie123@gmail.com>
2025-04-04 15:00:28 +0900

    共通型定義 ReceiptItem を追加

---
+++ src/types/receipt.ts
@@ -0,0 +1,12 @@
+export interface ReceiptItem {
+  id: string;
+  imageUrl: string;
+  pdfUrl: string;
+  date: string;
+  vendor: string;
+  amount: number;
+  type: string;
+  memo: string;
+  tag: string;
+  status: string;
+}
